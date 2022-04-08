30c3a043044d166838040c07e9a402c8a3a701d8
Akshayku005 <akshaysportive@gmail.com>
2022-04-08 18:23:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Day8Employee_Wage" 
---
+++ Uc1.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Uc1 {
+	static int random(){
+		Random ran = new Random();       // creating random number 0and 1
+		int i = ran.nextInt(2);
+		return i;
+	}
+
+	static String attendence(){
+		String atten;
+		if (random()==1){                  //calling random()
+			atten = "Present";
+		}
+		else   {
+			atten = "Absent";
+		}
+		return atten;
+	}
+
+
+	public static void main(String[] args){
+		System.out.println("The Employee is "+attendence());
+	}
+}
+
